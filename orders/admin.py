81d29dbe0c9a4790aaf9d31c926d4c347468d1c4
emmanueluwa <emmanuel@fulodev.com>
2022-09-26 19:35:44 +0100

    adding ability to visit stripe payment dashboard based on order in django admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from. models import Order, OrderItem
+from django.utils.safestring import mark_safe
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
+
+def order_stripe_payment(obj):
+    url = obj.get_stripe_url()
+    if obj.stripe_id:
+        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+        #django escapes html output by default
+        return mark_safe(html)
+    return ''
+order_stripe_payment.short_description = 'Stripe payment'
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
@@ -16,6 +26,7 @@ class OrderAdmin(admin.ModelAdmin):
         'post_code',
         'city',
         'paid',
+        order_stripe_payment,
         'created',
         'updated'
     ]
--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,7 @@
 import email
 from django.db import models
 from shop.models import Product
+from django.conf import settings
 
 class Order(models.Model):
     first_name = models.CharField(max_length=50)
@@ -25,6 +26,16 @@ class Order(models.Model):
     
     def get_total_cost(self):
         return sum(item.get_cost() for item in self.items.all())
+    
+    #url to stripe stripe dashboard payment details
+    def get_stripe_url(self):
+        if not self.stripe_id:
+            return ''
+        if '_test_' in settings.STRIPE_SECRET_KEY:
+            path = '/test/'
+        else:
+            path ='/'
+        return f'https://dashboard.stripe.com{path}payments/{self.stripe_id}'
 
 
 class OrderItem(models.Model):
